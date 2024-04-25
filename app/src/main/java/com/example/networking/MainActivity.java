5761403e2cf0e834d91896f5650ec4b0e767516d
a23fanro <a23fanro@hi>
2024-04-25 14:56:23 +0200

    LAde till ArrayList<Mountain> som en member variabel "varMountain"

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,11 +5,15 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
+
+    private ArrayList<Mountain> varMountain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +21,13 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+
+        varMountain = new ArrayList<>();
     }
 
     @Override
     public void onPostExecute(String json) {
+
         Log.d("MainActivity", json);
     }
 
